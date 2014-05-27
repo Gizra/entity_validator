8b68383e11e79cb39a6ed038128725f3f060dfcb
RoySegall <roy.segall@gizra.com>
2014-05-27 17:39:55 +0300

    Imrpove the example module.

--- modules/entity_validator_example/plugins/validator/node/article/NodeValidatorExampleArticleValidator.php
+++ modules/entity_validator_example/plugins/validator/node/article/NodeValidatorExampleArticleValidator.php
@@ -3,6 +3,9 @@
 class NodeValidatorExampleArticleValidator extends NodeValidate {
 
   public function validate() {
+    if (in_array($this->fields['title'], array('foo', 'bar'))) {
+      $this->setError(t("The title of the article can't bee foo or bar"));
+    }
     parent::validate();
   }
 }
