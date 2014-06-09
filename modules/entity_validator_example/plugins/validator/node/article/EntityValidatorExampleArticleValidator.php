c96685d935938548dc3e493a1899858cda0701f5
Amitaibu <amitai@gizra.com>
2014-06-09 15:09:53 +0300

    More cleanup.

---
+++ modules/entity_validator_example/plugins/validator/node/article/EntityValidatorExampleArticleValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains EntityValidatorExampleArticleValidator.
+ */
+
+class EntityValidatorExampleArticleValidator extends NodeValidate {
+
+  /**
+   * Overrides NodeValidate::getFieldsInfo().
+   */
+  public function getFieldsInfo() {
+    $fields = parent::getFieldsInfo();
+
+    $fields['field_date'] = array(
+      'validators' => array(
+        array($this, 'isUnixTimeStamp'),
+      ),
+      'preprocess' => array(
+        array($this, 'preprocessDate'),
+      ),
+    );
+
+    return $fields;
+  }
+}
