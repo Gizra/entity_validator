55a821d1eac9bf435096fc90db0324edf3672a65
Roy Segall <roy.segall@gizra.com>
2014-12-14 13:57:27 +0200

    Adding exmaple to the validation proccess.

--- modules/entity_validator_example/plugins/validator/entity_validator_example/EntityValidatorExampleObjectValidator.class.php
+++ modules/entity_validator_example/plugins/validator/entity_validator_example/EntityValidatorExampleObjectValidator.class.php
@@ -1,5 +1,44 @@
 <?php
 
 class EntityValidatorExampleObjectValidator extends ObjectValidateBase {
-  
+
+  /**
+   * Overrides ObjectValidateBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $fields = parent::publicFieldsInfo();
+
+    $fields['title']['validators'][] = array($this, 'validateTitleText');
+    $fields['created']['validators'][] = array($this, 'validateUnixTimeStamp');
+
+    return $fields;
+  }
+
+  /**
+   * Validate the title is at least 3 characters long.
+   *
+   * @param string $property
+   *   The field name.
+   * @param mixed $value
+   *   The value of the field.
+   */
+  public function validateTitleText($property, $value) {
+    if (strlen($value) < 3) {
+      $this->setError($property, 'The @property property should be at least 3 characters long.');
+    }
+  }
+
+  /**
+   * Validate the unix time stamp format.
+   *
+   * @param string $property
+   *   The field name.
+   * @param mixed $value
+   *   The value of the field.
+   */
+  public function validateUnixTimeStamp($property, $value) {
+    if ($value < 7200) {
+      $this->setError($property, 'The @property property should be a valid unix time stamp.');
+    }
+  }
 }
