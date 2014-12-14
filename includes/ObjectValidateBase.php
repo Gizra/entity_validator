3c017c14b9a4c97c8cb481c4543bcbcb284ab2d2
Roy Segall <roy.segall@gizra.com>
2014-12-14 12:57:02 +0200

    More code!

--- includes/ObjectValidateBase.php
+++ includes/ObjectValidateBase.php
@@ -15,6 +15,26 @@ class ObjectValidateBase implements ObjectValidateInterface {
   protected $plugin;
 
   /**
+   * @var Array
+   * List of errors collected in the validation process.
+   */
+  protected $errors;
+
+  /**
+   * @return Array
+   */
+  public function getPlugin() {
+    return $this->plugin;
+  }
+
+  /**
+   * @param Array $plugin
+   */
+  public function setPlugin($plugin) {
+    $this->plugin = $plugin;
+  }
+
+  /**
    * @param Array $schema
    *   The schema information.
    *
@@ -37,7 +57,7 @@ class ObjectValidateBase implements ObjectValidateInterface {
    * {@inheritdoc}
    */
   public function __construct($plugin) {
-    $this->plugin = $plugin;
+    $this->setPlugin($plugin);
 
     if (empty($this->plugin['schema'])) {
       throw new \EntityValidatorException('Missing schema name.');
@@ -47,6 +67,7 @@ class ObjectValidateBase implements ObjectValidateInterface {
     if (!$schema || !db_table_exists($plugin['schema'])) {
       throw new \EntityValidatorException(format_string('A schema or a definition for @name was not found.', array('@name' => $plugin['schema'])));
     }
+
     $this->setSchema($schema);
   }
 
@@ -60,9 +81,14 @@ class ObjectValidateBase implements ObjectValidateInterface {
     foreach ($schema['fields'] as $field => $info) {
       $fields[$field] = array(
         'type' => $this->getRealType($info['type']),
+        'required' => $info['not null'],
       );
+
+      if (!empty($info['size'])) {
+        $fields[$field]['size'] = $info['size'];
+      }
     }
-    
+
     return $fields;
   }
 
@@ -92,8 +118,14 @@ class ObjectValidateBase implements ObjectValidateInterface {
    */
   public function getPublicFields() {
     $public_fields = $this->publicFieldsInfo();
-    foreach ($public_fields as $property => &$public_field) {
 
+    foreach ($public_fields as $property => &$public_field) {
+      // Adding type validation.
+      $public_field['callbacks'][] = array($this, 'validateType');
+
+      if ($public_field['required']) {
+        $public_field['callbacks'][] = array($this, 'isNotEmpty');
+      }
     }
 
     return $public_fields;
@@ -109,20 +141,35 @@ class ObjectValidateBase implements ObjectValidateInterface {
     if (!$public_fields = $this->getPublicFields()) {
       return TRUE;
     }
+
+    dpm($public_fields);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function setError($property_name, $message, $params = '') {
-    // TODO: Implement setError() method.
+  public function setError($field_name, $message, $params = array()) {
+    $params['@field'] = $field_name;
+    $this->errors[$field_name][] = array('message' => $message, 'params' => $params);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getErrors($squash = TRUE) {
-    // TODO: Implement getErrors() method.
+    if (!$squash) {
+      return $this->errors;
+    }
+
+    $return = array();
+    foreach ($this->errors as $errors) {
+      foreach ($errors as $error) {
+        $error += array('params' => array());
+        $return[] = format_string($error['message'], $error['params']);
+      }
+    }
+
+    return implode("\n\r", $return);
   }
 
   /**
